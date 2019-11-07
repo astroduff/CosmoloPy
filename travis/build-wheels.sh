8d72874ebe42a71d7e74ad566d22d8213bcd7d3d
Ellert van der Velden <ellert_vandervelden@outlook.com>
2019-11-07 14:28:46 +1100

    Only use Python 2.7/3.5+.

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -5,7 +5,7 @@ set -e -x
 yum install -y swig
 
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
+for PYBIN in /opt/python/*[23][5678]*/bin; do
     "${PYBIN}/pip" install -r /io/requirements.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
@@ -16,7 +16,7 @@ for whl in wheelhouse/cosmolopy*.whl; do
 done
 
 # Install packages and test
-for PYBIN in /opt/python/*/bin/; do
+for PYBIN in /opt/python/*[23][5678]*/bin/; do
     "${PYBIN}/pip" install cosmolopy --no-index -f /io/wheelhouse
     "${PYBIN}/python" -c "import cosmolopy; import cosmolopy.EH.power"
 done
