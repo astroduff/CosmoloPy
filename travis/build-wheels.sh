219145b1264d1887bc56f1c8bfbcd7b6d53793c3
Ellert van der Velden <ellert_vandervelden@outlook.com>
2019-11-08 12:09:05 +1100

    Finally getting somewhere.

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -8,8 +8,7 @@ echo $(which swig)
 yum localinstall -y http://springdale.math.ias.edu/data/puias/computational/6/x86_64//swig3012-3.0.12-3.sdl6.x86_64.rpm
 yum list installed
 yum info swig3012
-yum install -y yum-utils
-repoquery --list swig3012
+rpm -ql swig
 rpm -ql swig3012
 echo $(which swig3012.x86_64)
 printf '%s\n' ${PATH//:/\/* } | grep swig
